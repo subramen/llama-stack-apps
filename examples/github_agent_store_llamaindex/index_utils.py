cecdb3e2780461d44222ff6f78137eda095f39cf
Suraj Subramanian <suraj813@gmail.com>
2024-11-26 09:35:36 -0500

    refactor, replace low-level with stable API

--- examples/github_agent_store_llamaindex/index_utils.py
+++ examples/github_agent_store_llamaindex/index_utils.py
@@ -5,7 +5,7 @@ from typing import Callable, List, Optional, Tuple
 import fire
 import requests
 
-from llama_index.core import Document, Settings, VectorStoreIndex
+from llama_index.core import Document, SummaryIndex, VectorStoreIndex
 from llama_index.core.node_parser import TokenTextSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from smart_open import open as smart_open
@@ -113,7 +113,7 @@ def get_directory_files(
     return docs
 
 
-def create_index(docs: List[Document], persist=True):
+def create_indexes(docs: List[Document], persist=True):
     # index = VectorStoreIndex.from_documents(docs)  # equivalent to memory_banks.register() + memory.insert()
     splitter = TokenTextSplitter(
         chunk_size=512,
@@ -122,7 +122,13 @@ def create_index(docs: List[Document], persist=True):
     )
     nodes = splitter.get_nodes_from_documents(docs)
     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-    index = VectorStoreIndex(nodes, embed_model=embed_model)  # customizability
+    vector_index = VectorStoreIndex(nodes, embed_model=embed_model)
+    summary_index = SummaryIndex(nodes, embed_model=embed_model)
     if persist:
-        index.storage_context.persist(persist_dir="./cache")
-    return index
+        vector_index.storage_context.persist(persist_dir="./cache")
+        summary_index.storage_context.persist(persist_dir="./cache")
+
+    return {
+        "vector_index": vector_index,
+        "summary_index": summary_index,
+    }
--- examples/github_agent_store_llamaindex/main.py
+++ examples/github_agent_store_llamaindex/main.py
@@ -1,27 +1,14 @@
-from agent_utils import *
-from index_utils import *
+import os
 
+from dotenv import load_dotenv
+from index_utils import create_indexes, get_directory_files, get_github_repo_files
 from llama_index.core import PromptTemplate
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.response_synthesizers import TreeSummarize
-from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.llms.groq import Groq
 
-from dotenv import load_dotenv
 load_dotenv()
-
-
-## [P1] Given a repository, can I identify all the resources associated with it?
-
-## [P0] Index documentation on separate memory bank
-### recursive scrape of website
-
-## [P0] Index code on separate memory bank
-### github recursive thru repo
-
-## [P0] Create agent associated with both memory banks
-
-## [P1] Insert new document in appropriate memory bank
 
 
 def test_setup_index_files(directory):
@@ -30,8 +17,8 @@ def test_setup_index_files(directory):
     py = [d for d in docs if d.metadata["extension"] == "py"]
     md = [d for d in docs if d.metadata["extension"] == "md"]
     return {
-        "py": create_index(py, persist=True),
-        "md": create_index(md, persist=True),
+        "py": create_indexes(py, persist=True),
+        "md": create_indexes(md, persist=True),
     }
 
 
@@ -41,12 +28,14 @@ def test_setup_index_gh(owner, repo):
     py = [d for d in docs if d.metadata["extension"] == "py"]
     md = [d for d in docs if d.metadata["extension"] == "md"]
     return {
-        "py": create_index(py, persist=True),
-        "md": create_index(md, persist=True),
+        "py": create_indexes(py, persist=True),
+        "md": create_indexes(md, persist=True),
     }
 
 
 def test_setup_rag_ensemble(indexes):
+    groq_llm = Groq(model="llama-3.2-3b-preview", api_key=os.getenv("GROQ_API_KEY"))
+
     qa_prompt_tmpl = (
         "Here is some context information gathered from an open-source repo's {filetype} files:\n"
         "---------------------\n"
@@ -54,26 +43,54 @@ def test_setup_rag_ensemble(indexes):
         "---------------------\n"
         "Given the context information and not prior knowledge, "
         "answer the query.\n"
-        "Please also write the answer in the tone of {tone_name}.\n"
+        "Please also write the answer in the tone of a professional software developer.\n"
         "Query: {query_str}\n"
         "Answer: "
     )
     qa_prompt = PromptTemplate(qa_prompt_tmpl)
 
     rag_ensemble = {}
-    for name, index in indexes.items():
-        retriever = VectorIndexRetriever(index=index, similarity_top_k=10, filters=None)
+    for name, indexes in indexes.items():
+        summary_index = indexes["summary_index"]
+        summary_query_engine = summary_index.as_query_engine(llm=groq_llm)
+
+        vector_index = indexes["vector_index"]
+        retriever = vector_index.as_retriever(similarity_top_k=10)
         synthesizer = TreeSummarize(
-            llm=Groq(
-                model="llama-3.2-3b-preview",
-                api_key=os.getenv("GROQ_API_KEY"),
-            ),
+            llm=groq_llm,
             summary_template=qa_prompt,
             verbose=True,
         )
-        rag_ensemble[name] = RetrieverQueryEngine(
+        vector_query_engine = RetrieverQueryEngine(
             retriever=retriever, response_synthesizer=synthesizer
         )
+
+        rag_tools = [
+            QueryEngineTool(
+                query_engine=vector_query_engine,
+                metadata=ToolMetadata(
+                    name="vector_tool",
+                    description=(
+                        "Useful for questions related to specific aspects of"
+                        f" {name} documents."
+                    ),
+                ),
+            ),
+            QueryEngineTool(
+                query_engine=summary_query_engine,
+                metadata=ToolMetadata(
+                    name="summary_tool",
+                    description=(
+                        "Useful for any requests that require a holistic summary"
+                        f" of EVERYTHING about the {name} type document. For questions about"
+                        " more specific sections, please use the vector_tool."
+                    ),
+                ),
+            ),
+        ]
+
+        agent = ReActAgent(tools=rag_tools, llm=groq_llm)
+
     return rag_ensemble
 
 
@@ -86,10 +103,14 @@ def test_query_ensemble(agent_store, query):
     for name, agent in agent_store.items():
         print(f"Querying {name}...\n===========\n")
         knn_chunks = agent.retrieve(query)  # ~ client.memory.query(query)
-        chunk_str = [n.get_text() for n in knn_chunks]
-        answer = agent._response_synthesizer.get_response(
-            query, chunk_str, tone_name="a professional software developer"
-        )
+        answer = agent.synthesize(query, knn_chunks)
+
+        #### low-level API to pass addl args to custom_template ####
+        # chunk_str = [n.get_text() for n in knn_chunks]
+        # answer = agent._response_synthesizer.get_response(
+        #     query, chunk_str, tone_name="a professional software developer"
+        # )
+
         ensemble_responses[name] = {
             "query": query,
             "completion": answer,
